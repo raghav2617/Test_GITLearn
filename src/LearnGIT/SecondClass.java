28fd06907da6bc939d1c4aefbd02d4baada30745
raghav2617 <55792238+raghav2617@users.noreply.github.com>
2020-06-29 18:31:21 +0530

    Commit my Summu kaur

--- src/LearnGIT/SecondClass.java
+++ src/LearnGIT/SecondClass.java
@@ -4,7 +4,7 @@ public class SecondClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I am learning GIT HUB.");
+		System.out.println("SUmmu is best");
 	}
 
 }
