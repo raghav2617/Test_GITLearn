ad244a268e1898a09b5ab9506a4f49a68fd7d2bc
raghav2617 <55792238+raghav2617@users.noreply.github.com>
2020-06-29 18:36:40 +0530

    Second commit by SKN

--- src/LearnGIT/FirstClass.java
+++ src/LearnGIT/FirstClass.java
@@ -7,5 +7,6 @@ public class FirstClass {
 		System.out.println("I am learning GIT HUB.");
 		System.out.println("I am learning GIT.");
 		System.out.println("My name is Raghav");
+		System.out.println("Summu Loves Motti Churr laddu.");
 	}
 }
