71dd2210b31082803c2228d65324a83b0ef636d2
raghav2617 <55792238+raghav2617@users.noreply.github.com>
2020-06-29 18:49:19 +0530

    Commit by Sharma Ji

--- src/LearnGIT/FirstClass.java
+++ src/LearnGIT/FirstClass.java
@@ -6,7 +6,7 @@ public class FirstClass {
 		
 		System.out.println("I am learning GIT HUB.");
 		System.out.println("I am learning GIT.");
-		System.out.println("My name is Raghav");
 		System.out.println("Summu Loves Motti Churr laddu.");
+		System.out.println("Ayush likes LOLO");
 	}
 }
